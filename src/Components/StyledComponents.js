d1c51f83179b076e1a4b31a44ebfaf8c92c04e7a
MirkusX <marcusk.kodehode@gmail.com>
2023-02-20 13:55:43 +0100

    styled desktop & mobile

--- src/Components/StyledComponents.js
+++ src/Components/StyledComponents.js
@@ -1,1 +1,32 @@
 import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+  background-color: blue;
+  width: 25%;
+  padding: 1em;
+  border-radius: 10px;
+  color: gray;
+  box-shadow: 0px 0px 39px -14px rgba(0, 0, 0, 0.75);
+  @media (max-width: 811px) {
+    width: 90%;
+  }
+`;
+
+export const StyledImg = styled.img`
+  border-radius: 10px;
+`;
+
+export const StyledSection = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  @media (max-width: 811px) {
+    align-items: flex-start;
+    margin-top: 1em;
+  }
+`;
+
+export const StyledH1 = styled.h1`
+  color: white;
+`;
--- src/Pages/Frontpage.js
+++ src/Pages/Frontpage.js
@@ -1,9 +1,25 @@
+import {
+  StyledDiv,
+  StyledH1,
+  StyledImg,
+  StyledSection,
+} from "../Components/StyledComponents";
+import qr from "../Images/frame.png";
+
 export const Frontpage = () => {
   return (
-    <section>
-      <div>
-        <h1>QR</h1>
-      </div>
-    </section>
+    <StyledSection>
+      <StyledDiv>
+        <StyledImg src={qr} />
+        <div>
+          <StyledH1>Scan the QR code to visit my portfolio</StyledH1>
+          <p>
+            The original text said something about scanning the QR code to visit
+            their site so you could do their challenges but as the Joker said if
+            you're good at something never do it for free.
+          </p>
+        </div>
+      </StyledDiv>
+    </StyledSection>
   );
 };
